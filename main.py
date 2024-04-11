a607c50a7f9df8881d05c1df8672ce95b1c54204
EthanGReinhart <ereinhart23@gmail.com>
2024-04-11 10:36:16 -0400

    decode function

--- main.py
+++ main.py
@@ -5,8 +5,16 @@ def encode(password):
         new_digit = input_array[i] + 3
         input_array[i] = new_digit % 10
     result_string = ''.join(map(str, input_array))
+    return result_string
 
 
+#Ethan Reinhart
+def decode(saved_pass):
+    input_array = [int(char) for char in password]
+    for i in range(len(input_array)):
+        new_digit = input_array[i] -3
+    orginal_password = ''.join(map(str, input_array))
+    return orginal_password
 
 def console():
     print("menu")
@@ -31,5 +39,11 @@ while running == 1:
                 password = input("please enter your password to encode")
             saved_pass = encode(password)
             print("Your password has been encoded and stored!")
+        elif menu_value == 2:
+            try:
+                decoded = decode(saved_pass)
+                print(f"The encoded password is {saved_pass}, and the orginal password is, {decoded}")
+            except:
+                print("No stored password")
         if menu_value == 3:
             break
